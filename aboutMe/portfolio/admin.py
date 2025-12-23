cfe54ecadf19769998d62d6fa1d8ed6aedbe5e00
john <johngezae@yahoo.com>
2025-12-23 21:47:43 +1100

    remove aboutme project

--- aboutMe/portfolio/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Project, Tool
-# Register your models here.
-admin.site.register(Project)
-admin.site.register(Tool)
--- aboutMe/portfolio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('api/projects/', views.projects_api, name='projects_api'),
-    path('contact/', views.contact, name='contact'),
-]
