97dc8b6df09af285eec07a7413dc4f3ec8cf5865
Join Kalayu <113885259+Johnkalayu@users.noreply.github.com>
2025-12-30 23:28:26 +1100

    Remove default email from settings

--- project_portfolio/project_portfolio/settings.py
+++ project_portfolio/project_portfolio/settings.py
@@ -115,7 +115,7 @@ EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'johngezae@yahoo.com')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', '')
 
 # Site configuration
 SITE_CONFIG = {
